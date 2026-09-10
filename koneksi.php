d1eba99c6c9aea8ddc27a2e40b665c3522a9de6c
Wann1904 <iwan.vj421@example.com>
2026-09-10 14:10:45 +0700

    Upload website PKM

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,7 @@
 <?php
 $host     = "localhost";
-$user     = "Ridwanmp";      // Default username XAMPP
-$password = "1904";          // Default password XAMPP (kosong)
-$database = "db_user";   // Sesuaikan dengan nama database Anda di phpMyAdmin
+$user     = "Ridwanmp";      
+$database = "db_user";  
 
 $koneksi = mysqli_connect($host, $user, $password, $database);
 
